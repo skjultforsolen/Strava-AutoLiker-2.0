608fc8f3c291082fef46f6963d6e4420ffed6228
skjultforsolen <skjultforsolen@gmail.com>
2023-05-02 16:20:06 +0200

    add language choose
    add ENG and RUS language switch

--- popup.js
+++ popup.js
@@ -2,9 +2,32 @@ var autoClick;
 var timerId;
 var arg;
 var err;
+var titleVar;
 
-console.log("injected");
-
+function changeLangToRus() {
+    var buttonStart = document.getElementById("grabBtn");
+    var buttonStop = document.getElementById("StopBtn");
+    var changeYourLangLabel1 = document.getElementById("changeYourLangLabel1");
+    changeYourLangLabel1.textContent = "Выберите ваш язык"
+    buttonStart.textContent = "Запустить скрипт";
+    buttonStop.textContent = "Остановить скрипт";
+  }
+  
+  // вызываем функцию при нажатии на кнопку
+  document.getElementById("rusLangButt").addEventListener("click", changeLangToRus);
+  
+  function changeLangToEng() {
+    var buttonStart = document.getElementById("grabBtn");
+    var buttonStop = document.getElementById("StopBtn");
+    var changeYourLangLabel1 = document.getElementById("changeYourLangLabel1");
+    changeYourLangLabel1.textContent = "Choose your language"
+    buttonStart.textContent = "Start script";
+    buttonStop.textContent = "Stop script";
+  }
+  
+  // вызываем функцию при нажатии на кнопку
+  document.getElementById("engLangButt").addEventListener("click", changeLangToEng);
+  
 const grabBtn = document.getElementById("grabBtn");
 grabBtn.addEventListener("click",() => {    
     chrome.tabs.query({active: true}, function(tabs) {
@@ -58,7 +81,7 @@ function interval(args) {
 
   function set() {
       try {
-          autoClick = document.querySelector('button[title="Поставить зачет"]');
+          autoClick = document.querySelector(`button[title="${titleVar}"]`);
           autoClick.click();
           scrollingToSaveData = autoClick;
           scrollingToSaveData.scrollIntoView({behavior: "smooth", block: "center", inline: "start"});
@@ -66,12 +89,12 @@ function interval(args) {
           err = 0;
           }
         catch(e) {
-          console.log("Кнопка \"Лайк\" не найдена.")
+          console.log("Button \"Like\" is not found.")
           window.scrollTo({
             top: 999999999,
             behavior: "smooth"
         });
-          if(err >= 10) {
+          if(err >= 20) {
             timerId = clearInterval(timerId);
             console.log('Function has been stopped');
           }
